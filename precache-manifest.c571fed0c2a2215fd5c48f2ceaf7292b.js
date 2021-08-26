c56938e582c08d5a7e07fd01129269b64855cf90
ymleong <ymleong@mit.com.sg>
2021-08-26 17:23:07 +0800

    Updates

---
+++ precache-manifest.c571fed0c2a2215fd5c48f2ceaf7292b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20663e634ea4691d78585918a7c3c60",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3a25822a289ea378979b",
+    "url": "/react-gh-pages/static/css/3.ce554f52.chunk.css"
+  },
+  {
+    "revision": "077070fcdfc14466a2be",
+    "url": "/react-gh-pages/static/css/4.c8ac464a.chunk.css"
+  },
+  {
+    "revision": "477f817054e0e1ed9e07",
+    "url": "/react-gh-pages/static/css/main.8a2e70a2.chunk.css"
+  },
+  {
+    "revision": "beab9a25b78a49a3e9ce",
+    "url": "/react-gh-pages/static/js/2.820a3557.chunk.js"
+  },
+  {
+    "revision": "3a25822a289ea378979b",
+    "url": "/react-gh-pages/static/js/3.6a7c0b4a.chunk.js"
+  },
+  {
+    "revision": "077070fcdfc14466a2be",
+    "url": "/react-gh-pages/static/js/4.6efb55d8.chunk.js"
+  },
+  {
+    "revision": "477f817054e0e1ed9e07",
+    "url": "/react-gh-pages/static/js/main.53847203.chunk.js"
+  },
+  {
+    "revision": "621fcac4a70c41723f0f",
+    "url": "/react-gh-pages/static/js/runtime-main.35fd52fb.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/react-gh-pages/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/react-gh-pages/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/react-gh-pages/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/react-gh-pages/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/react-gh-pages/static/media/developerActivity.a41f7848.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/react-gh-pages/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "d6a2dcd5cb04a2d44cca99b6acb5da7e",
+    "url": "/react-gh-pages/static/media/ibm_logo.d6a2dcd5.png"
+  },
+  {
+    "revision": "b1ae2adc77bafd0ab3f670d9ab06b5ca",
+    "url": "/react-gh-pages/static/media/jsFramework.b1ae2adc.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/react-gh-pages/static/media/manOnTable.a15c230b.svg"
+  },
+  {
+    "revision": "d73bb2bcddad315c67b738e10a345582",
+    "url": "/react-gh-pages/static/media/meta_pic.d73bb2bc.png"
+  },
+  {
+    "revision": "f041d99793f0c15a757a3d8b73d937a8",
+    "url": "/react-gh-pages/static/media/pierian-data-logo.f041d997.png"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/react-gh-pages/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "05b89315da86d827606f827e50c8e7ca",
+    "url": "/react-gh-pages/static/media/rahulshettyacademy-logo.05b89315.png"
+  },
+  {
+    "revision": "1591c202ea86be03349f8d565ac1f959",
+    "url": "/react-gh-pages/static/media/seetest-logo.1591c202.png"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/react-gh-pages/static/media/skill.96c9d7a9.svg"
+  },
+  {
+    "revision": "7ebc764bf5f86dd0e65964bc4a734411",
+    "url": "/react-gh-pages/static/media/vskills-logo.7ebc764b.png"
+  },
+  {
+    "revision": "d0a70af7f35f5943c7e28883adbbb7fd",
+    "url": "/react-gh-pages/static/media/zero-to-mastery.d0a70af7.png"
+  }
+]);
